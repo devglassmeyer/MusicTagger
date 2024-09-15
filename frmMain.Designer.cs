ad93e3e0d4b165b429c297d9170a2cff94bd667f
Ray Glassmeyer <ray@digitalfamily.org>
2024-09-15 14:58:06 -0500

    commit number 1 - its the special first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MusicTagger
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtAlbumDir = new TextBox();
+            btnGetAlbumDir = new Button();
+            lstSongs = new ListBox();
+            label2 = new Label();
+            txtSongDetails = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Album Directory";
+            // 
+            // txtAlbumDir
+            // 
+            txtAlbumDir.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtAlbumDir.Location = new Point(10, 43);
+            txtAlbumDir.Name = "txtAlbumDir";
+            txtAlbumDir.Size = new Size(892, 23);
+            txtAlbumDir.TabIndex = 1;
+            // 
+            // btnGetAlbumDir
+            // 
+            btnGetAlbumDir.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnGetAlbumDir.Location = new Point(908, 43);
+            btnGetAlbumDir.Name = "btnGetAlbumDir";
+            btnGetAlbumDir.Size = new Size(26, 23);
+            btnGetAlbumDir.TabIndex = 2;
+            btnGetAlbumDir.Text = "...";
+            btnGetAlbumDir.UseVisualStyleBackColor = true;
+            btnGetAlbumDir.Click += btnGetAlbumDir_Click;
+            // 
+            // lstSongs
+            // 
+            lstSongs.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lstSongs.FormattingEnabled = true;
+            lstSongs.ItemHeight = 15;
+            lstSongs.Location = new Point(10, 107);
+            lstSongs.Name = "lstSongs";
+            lstSongs.Size = new Size(908, 229);
+            lstSongs.TabIndex = 3;
+            lstSongs.SelectedValueChanged += lstSongs_SelectedValueChanged;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(10, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Songs";
+            // 
+            // txtSongDetails
+            // 
+            txtSongDetails.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtSongDetails.Location = new Point(10, 363);
+            txtSongDetails.Multiline = true;
+            txtSongDetails.Name = "txtSongDetails";
+            txtSongDetails.Size = new Size(908, 287);
+            txtSongDetails.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(946, 662);
+            Controls.Add(txtSongDetails);
+            Controls.Add(label2);
+            Controls.Add(lstSongs);
+            Controls.Add(btnGetAlbumDir);
+            Controls.Add(txtAlbumDir);
+            Controls.Add(label1);
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "The Music Tagger";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtAlbumDir;
+        private Button btnGetAlbumDir;
+        private ListBox lstSongs;
+        private Label label2;
+        private TextBox txtSongDetails;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicTagger
+{
+    public partial class frmMain : Form
+    {
+        /// <summary>
+        /// True if we are loading data into controls; check if we don't want to do any processing while loading
+        /// </summary>
+        bool _loading_data = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetAlbumDir_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select the parent folder folder";
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                folderBrowserDialog.ShowNewFolderButton = true;
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    txtAlbumDir.Text = selectedPath;
+                    get_songs_in_dir(selectedPath);
+                }
+            }
+        }
+        private void lstSongs_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (_loading_data) return;
+
+            if (lstSongs.SelectedItem != null)
+            {
+                get_song_details();
+            }
+        }
+
+        private void get_song_details()
+        {
+            string full_song_path = get_selected_filename();
+            if ((full_song_path == null) || (full_song_path.Trim() == "")) return;
+
+            txtSongDetails.Text = "";
+
+            txtSongDetails.Text = "Song: " + full_song_path + "\r\n";
+        }
+
+        private void get_songs_in_dir(string song_dir)
+        {
+            if ((song_dir == null) || (song_dir.Trim() == "")) return;
+            if (System.IO.Path.Exists(song_dir))
+            {
+                _loading_data = true;
+                lstSongs.Items.Clear();
+                string[] files_in_dir = Directory.GetFiles(song_dir);
+                if (files_in_dir.Length > 0)
+                {
+                    foreach (string a_file in files_in_dir)
+                    {
+                        if (a_file.ToLower().EndsWith(".flac"))
+                        {
+                            lstSongs.Items.Add(Path.GetFileName(a_file));
+                        }
+                    }
+                }
+                _loading_data = false;
+            }
+
+        }
+
+        private string get_selected_filename()
+        {
+            if (lstSongs.SelectedItems.Count == 0) return "";
+
+            string full_file_name = System.IO.Path.Combine(txtAlbumDir.Text, lstSongs.SelectedItem.ToString());
+
+            if (System.IO.File.Exists(full_file_name))
+            {
+                return full_file_name;
+            }
+            return "";
+        }
+
+
+    }
+}
